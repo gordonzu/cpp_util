e29301bd9b3d2ddb91aed93b56068ade6ff9adde
gordonzu <gzulux@gmail.com>
2017-04-27 12:15:02 -0400

    Initial_commit

---
+++ exception.hpp
@@ -0,0 +1,56 @@
+// exception.hpp
+
+#include <exception>
+#include <system_error>
+#include <future>
+#include <iostream>
+
+namespace util
+{
+  template <typename T>
+  void processCodeException(const T& e)
+  {
+    auto c = e.code();
+    std::cerr << "- category:      " << c.category().name() << std::endl;
+    std::cerr << "- value:         " << c.value() << std::endl;
+    std::cerr << "- msg:           " << c.message() << std::endl;
+    std::cerr << "- def category:  " 
+              << c.default_error_condition().category().name() << std::endl;
+    std::cerr << "- def value:     "
+              << c.default_error_condition().value() << std::endl;
+    std::cerr << "- def msg:       "
+              << c.default_error_condition().message() << std::endl;
+
+  }
+
+  void processException()
+  {
+    try {
+      throw; // rethrow exception to deal with it here
+    }
+    catch (const ios_base::failure& e) {
+      cerr << "I/O EXCEPTION: " << e.what() << endl;
+      processCodeException(e);
+    }
+    catch (const system_error& e) {
+      cerr << "SYSTEM EXCEPTION: " << e.what() << endl;
+      processCodeException(e);
+    }
+    catch (const future_error& e) {
+      cerr << "FUTURE EXCEPTION: " << e.what() << endl;
+      processCodeException(e);
+    }
+    catch (const bad_alloc& e) {
+      cerr << "BAD ALLOC EXCEPTION: " << e.what() << endl;
+    }
+    catch (const exception& e) {
+      cerr << "EXCEPTION: " << e.what() << endl;
+    }
+    catch (...) {
+      cerr << "EXCEPTION (unknown)" << endl;
+    }
+  }
+}
+
+
+
